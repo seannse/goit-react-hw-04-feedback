971770a4db802fdf65cbba55b6c573e0cbe376d6
seannse <seannse@gmail.com>
2023-02-16 11:20:12 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ export function App() {
     <div>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={Object.keys({ good, neutral, bad })}
           onLeaveFeedback={updateState}
         />
       </Section>
